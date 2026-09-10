ca268cf54e56eeebaddbd5f62d5afe5868f5df8d
ryan4285 <rlucero617@gmail.com>
2026-09-10 11:25:44 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='denver-dead-v46';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html?v=46','./manifest.webmanifest'])))});
+const CACHE='denver-dead-v47';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html?v=47','./manifest.webmanifest','./denver-dead-preview.jpg'])))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('denver-dead-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{let copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html?v=46',copy));return r}).catch(()=>caches.match('./index.html?v=46')));return}e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{let copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html?v=47',copy));return r}).catch(()=>caches.match('./index.html?v=47')));return}e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
